6f7f2a63c02888270ce1a4df4d0e4b7932166a16
dsnaruka <devendra.singh@bullseyewebsites.co.uk>
2021-12-08 13:22:45 +0530

    My First commit

---
+++ app/design/frontend/Hammer/Usa/registration.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Keeler USA theme for Magento
+ *
+ * @package     Keeler
+ * @author      Keeler (https://www.keelerusa.com/)
+ * @copyright   Copyright 2017 Keeler (https://www.keelerusa.com/)
+ * @license     OSL
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/Hammer/Usa',
+    __DIR__
+);
